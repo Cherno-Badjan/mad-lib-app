2fdf29ab3fd2ecb362c2c77a17bcef7af535fc6d
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-08 17:54:45 -0800

    grab dom elements for inputs and add event listener

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 // import functions and grab DOM elements
+const nounInput1 = document.getElementById('input-noun');
+const nounInput2 = document.getElementById('input-noun-2');
+const adjectiveInput1 = document.getElementById('input-adjective');
+const adjectiveInput2 = document.getElementById('input-adjective-2');
+const verbInput1 = document.getElementById('input-verb');
+const verbInput2 = document.getElementById('input-verb-2');
+const verbInput3 = document.getElementById('input-verb-3');
+const verbInput4 = document.getElementById('input-verb-4');
+const verbInput5 = document.getElementById('input-verb-5');
 
-// initialize state
 
+const submitButton = document.getElementById('submit');
+
+const storyDisplayElement = document.getElementById('story');
 // set event listeners to update state and DOM
+
+submitButton.addEventListener('click', function(){
+    
+    //stuff 
+
+});
